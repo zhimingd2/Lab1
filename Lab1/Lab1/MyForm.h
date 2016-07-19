80a1e27f66c0511a0c72bfbd4cd592fb2619d401
zhiming <zhiming.dai@gmail.com>
2016-07-19 13:36:06 -0400

    Load images

--- Lab1/Lab1/MyForm.h
+++ Lab1/Lab1/MyForm.h
@@ -34,6 +34,11 @@ namespace Lab1 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::PictureBox^  pictureBox1;
+			 System::Drawing::Graphics^ g;
+			 Bitmap^ bmp;
+	protected:
+	private: System::Windows::Forms::Button^  button1;
 
 	private:
 		/// <summary>
@@ -48,12 +53,56 @@ namespace Lab1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Location = System::Drawing::Point(-5, 0);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(415, 466);
+			this->pictureBox1->TabIndex = 0;
+			this->pictureBox1->TabStop = false;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(416, 209);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(72, 37);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"Show/Hide";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(495, 464);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->pictureBox1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			this->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &MyForm::MyForm_Paint);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+		pictureBox1->Visible = !pictureBox1->Visible;
+		Refresh();
+	}
+	private: System::Void MyForm_Load(System::Object^  sender, System::EventArgs^  e) {
+		g = pictureBox1->CreateGraphics();
+		bmp = gcnew Bitmap(L"tiger.jpg");
+	}
+
+	private: System::Void MyForm_Paint(System::Object^  sender, System::Windows::Forms::PaintEventArgs^  e) {
+		g->DrawImage(bmp, 0, 0);
+	}
 	};
 }
